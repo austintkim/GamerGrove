9747abd02fae157dc8eb07098073a2023e70c96d
Austin Kim <mambafinisher@gmail.com>
2024-08-08 08:06:23 -0700

    Adding base back to vite.config.js

--- ghi/vite.config.js
+++ ghi/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  base: mode === 'production' ? '/gamer-grove' : '/',
   server: {
     host: true,
     strictPort: true,
