1ab23e5cc4ea36ce76ee4cb9d1bd576142be5f3f
Austin Kim <mambafinisher@gmail.com>
2024-08-08 08:34:59 -0700

    Moving error message down

--- ghi/src/main.jsx
+++ ghi/src/main.jsx
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-console.log('Reached main.jsx!')
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 )
+console.log('Reached main.jsx!')
