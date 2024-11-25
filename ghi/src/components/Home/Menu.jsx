9618568600404c7700c0b9481acced66a7a8e7d6
Austin Kim <mambafinisher@gmail.com>
2024-11-25 09:13:50 -0800

    Fixed clickable console filters in side menu

--- ghi/src/components/Home/Menu.jsx
+++ ghi/src/components/Home/Menu.jsx
@@ -30,53 +30,96 @@ const SideMenu = ({  }) => {
     return (
             <div className="side-menu">
                 <ul>
+<ul>
+    <Link to="/">
+        <h5 className='home' style={{ fontFamily: 'K2D' }}>Home Page</h5>
+    </Link>
+    <div className="small-space"></div>
 
-                    <Link to="/">
-                    <h5 className='home' style={{ fontFamily: 'K2D'}}>Home Page</h5>
-                    </Link>
-                    <div className="small-space"></div>
+    <hr className='solid' />
 
-                    <hr className='solid' />
+    <div className="small-space"></div>
 
-                    <div className="small-space"></div>
+    <NavLink to="/games" state={{ state: '' }} style={{ fontFamily: 'K2D' }}>All Games</NavLink>
+    <div className="small-space"></div>
+    <p>Consoles</p>
+    <hr className='solid' />
+        <ul>
+            <li
+                className="linkside"
+                style={{
+                    display: 'block',
+                    padding: 0,
+                    margin: 0,
+                    whiteSpace: 'nowrap',
+                    backgroundColor: 'transparent'
+                }}
+            >
+                <NavLink to="/games" state={{ state: 'xbox' }}>- Xbox</NavLink>
+            </li>
+            <li
+                className="linkside"
+                style={{
+                    display: 'block', 
+                    padding: 0,
+                    margin: 0,
+                    whiteSpace: 'nowrap',
+                    backgroundColor: 'transparent'
+                }}
+            >
+                <NavLink to="/games" state={{ state: 'playstation' }}>- PlayStation</NavLink>
+            </li>
+            <li
+                className="linkside"
+                style={{
+                    display: 'block',
+                    padding: 0,
+                    margin: 0,
+                    whiteSpace: 'nowrap',
+                    backgroundColor: 'transparent'
+                }}
+            >
+                <NavLink to="/games" state={{ state: 'pc' }}>- PC</NavLink>
+            </li>
+            <li
+                className="linkside"
+                style={{
+                    display: 'block',
+                    padding: 0,
+                    margin: 0,
+                    whiteSpace: 'nowrap',
+                    backgroundColor: 'transparent'
+                }}
+            >
+                <NavLink to="/games" state={{ state: 'nintendo' }}>- Nintendo</NavLink>
+            </li>
+        </ul>
+</ul>
 
-                    <NavLink to="/games" state={{ state: ''}} style={{ fontFamily: 'K2D'}}>All Games</NavLink>
-                    <div className="small-space"></div>
-                    <p>Consoles</p>
-                    <hr className='solid' />
-                    <ul>
-                    <li className='linkside'><NavLink to="/games" state={{ state: 'xbox' }} >- Xbox</NavLink></li>
-                    <li className='linkside'><NavLink to="/games" state={{ state: 'playstation' }} >- PlayStation</NavLink></li>
-                    <li className='linkside'><NavLink to="/games" state={{ state: 'pc' }} >- PC</NavLink></li>
-                    <li className='linkside'><NavLink to="/games" state={{ state: 'nintendo' }} >- Nintendo</NavLink></li>
-                    </ul>
+    <div className="small-space"></div>
+    <p>Genres</p>
+    <hr className='solid'/>
+    <ul>
 
+        {genres.map(genre => {
+            return(
+                <li key={genre} className='linkside'><NavLink to="/games" state={{ state: genre }} >- {genre}</NavLink></li>
+            )
 
+        })}
 
-                    <div className="small-space"></div>
-                    <p>Genres</p>
-                    <hr className='solid'/>
-                    <ul>
+    </ul>
+    <br />
+    <br />
+    <div className="small-space"></div>
+    <hr className='solid' />
 
-                        {genres.map(genre => {
-                            return(
-                                <li key={genre} className='linkside'><NavLink to="/games" state={{ state: genre }} >- {genre}</NavLink></li>
-                            )
+    <hr className='solid'/>
+    <ul>
 
-                        })}
-
-                    </ul>
-                    <br />
-                    <br />
-                    <div className="small-space"></div>
-                    <hr className='solid' />
-
-                    <hr className='solid'/>
-                    <ul>
-
-                    </ul>
-                </ul>
-            </div>
+    </ul>
+</ul>
+</div>
     );
 };
 
