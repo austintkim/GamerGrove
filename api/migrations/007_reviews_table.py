387b4f1b4be10ddadec3534c192c108f5a7e2849
Austin Kim <mambafinisher@gmail.com>
2024-12-09 11:40:34 -0800

    Added date_created and last_update fields to reviews table

--- api/migrations/007_reviews_table.py
+++ api/migrations/007_reviews_table.py
@@ -1,6 +1,5 @@
 steps = [
     [
-
         """
         CREATE TABLE reviews (
         id SERIAL PRIMARY KEY,
@@ -12,14 +11,27 @@ steps = [
         FOREIGN KEY (game_id) REFERENCES gamesdb(id),
         replies_count INT DEFAULT 0,
         upvote_count INT DEFAULT 0,
-        rating INT
+        rating INT,
+        date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
 
+        CREATE OR REPLACE FUNCTION update_last_update_column()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.last_update = CURRENT_TIMESTAMP;
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
 
+        CREATE TRIGGER set_last_update
+        BEFORE UPDATE ON reviews
+        FOR EACH ROW
+        EXECUTE FUNCTION update_last_update_column();
         """,
         """
         DROP TABLE reviews;
+        DROP FUNCTION update_last_update_column;
         """
     ],
-
 ]
