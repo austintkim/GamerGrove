bedc580283a01b30fe7d8221dedb43466998d2a2
Austin Kim <mambafinisher@gmail.com>
2025-01-01 12:05:50 -0800

    Add username field to reviews table

--- api/migrations/007_reviews_table.py
+++ api/migrations/007_reviews_table.py
@@ -7,6 +7,7 @@ steps = [
         title VARCHAR(255),
         account_id INT NOT NULL,
         FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE,
+        username VARCHAR(255),
         game_id INT NOT NULL,
         FOREIGN KEY (game_id) REFERENCES gamesdb(id),
         replies_count INT DEFAULT 0,
