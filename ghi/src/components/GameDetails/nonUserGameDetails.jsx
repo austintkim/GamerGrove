4f1eeebd813eea6f2e5fe0502499998b13739bee
Austin Kim <mambafinisher@gmail.com>
2025-01-14 13:54:04 -0800

    Change back to game details to back button for non-user

--- ghi/src/components/GameDetails/nonUserGameDetails.jsx
+++ ghi/src/components/GameDetails/nonUserGameDetails.jsx
@@ -20,7 +20,7 @@ function NonUserGameDetails() {
                 <h1>Either login or sign up to interact with this game! 🫶🫰</h1>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'center', gap: '10px', marginTop: '20px' }}>
-                <button onClick={() => navigate(`/games/${id}`)}> Back to Game Details </button>
+                <button onClick={() => navigate(-1)}> Back </button>
                 <button onClick={() => navigate('/signup')}> Sign Up </button>
                 <button onClick={() => navigate('/login')}> Log In </button>
                 </div>
