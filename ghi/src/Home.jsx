237d70efd861a62e08562c6685531cff781e5779
Austin Kim <mambafinisher@gmail.com>
2025-02-27 16:06:52 -0800

    Reverting back to 0.25 sec

--- ghi/src/Home.jsx
+++ ghi/src/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
     useEffect(() => {
         const timeout = setTimeout(() => {
             setShowLoader(true);
-        }, 380);
+        }, 250);
 
         return () => clearTimeout(timeout);
     }, []);
