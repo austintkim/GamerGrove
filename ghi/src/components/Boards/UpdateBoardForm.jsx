79abf430dd3561813a1dce88ba9721a3ee036368
Austin Kim <mambafinisher@gmail.com>
2025-03-04 17:01:23 -0800

    Replaced 8-bit board with rainbow update cloud on update a board page

--- ghi/src/components/Boards/UpdateBoardForm.jsx
+++ ghi/src/components/Boards/UpdateBoardForm.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import updateboard from '../../assets/updateboard.gif'
 
 const containerStyle = {
   minHeight: '100vh',
@@ -125,43 +126,123 @@ function UpdateBoardForm() {
   };
 
   return (
-    <div>
-      <Board />
-      <div style={{ position: 'relative', ...containerStyle }}>
-        <div style ={{ ...centerVertically, width: '100%'}}>
-          <div className="card text-bg-light mb-3">
-            <div className="container">
-              <div className="row"style={{ backgroundColor: 'transparent', paddingLeft: '0%', marginLeft: '0%', marginRight: '0%' }}>
-                <div className="offset-1 col-10">
-                  <h2 className="card-header" style={{ textAlign: 'center'}}>Update Board</h2>
-                    <div style={{ width: '100%'}}>
-                      <form onSubmit={handleSubmit} id="update-board">
-                        <div className="form-floating mb-3" style={{ textAlign: 'center'}}>
-                          <label htmlFor="board_name">Title</label>
-                          <input onChange={handleFormChange} required type="text" name="board_name" id="board_name" className="form-control" value={formData.board_name} />
-                        </div>
-                        <div className="form-floating mb-3" style={{ textAlign: 'center'}}>
-                          <label htmlFor="description">Description</label>
-                          <textarea onChange={handleFormChange} name="description" id="description" className="form-control" value={formData.description} rows="3"></textarea>
-                        </div>
-                        <div className="form-floating mb-3" style={{ textAlign: 'center'}}>
-                          <label htmlFor="cover_photo">Cover photo</label>
-                          <input onChange={handleFormChange} required type="url" name="cover_photo" id="cover_photo" className="form-control" value={formData.cover_photo} />
-                        </div>
-                      </form>
-                      <div className="d-flex justify-content-between">
-                        <button className="mb-3" style={{ textAlign: 'left'}} onClick={() => navigate(-1)}>Back</button>
-                        <button form="update-board" className="mb-3" style={{ textAlign: 'right'}}>Update</button>
+      <div>
+          <img
+              src={updateboard}
+              alt=""
+              style={{
+                  position: 'fixed',
+                  bottom: '67%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  width: '250px',
+                  objectFit: 'contain',
+                  cursor: 'pointer',
+                  padding: '16px',
+                  zIndex: 3,
+              }}
+          />
+          <div style={{ position: 'relative', ...containerStyle }}>
+              <div style={{ ...centerVertically, width: '100%' }}>
+                  <div className="card text-bg-light mb-3">
+                      <div className="container">
+                          <div
+                              className="row"
+                              style={{
+                                  backgroundColor: 'transparent',
+                                  paddingLeft: '0%',
+                                  marginLeft: '0%',
+                                  marginRight: '0%',
+                              }}
+                          >
+                              <div className="offset-1 col-10">
+                                  <h2
+                                      className="card-header"
+                                      style={{ textAlign: 'center' }}
+                                  >
+                                      Update Board
+                                  </h2>
+                                  <div style={{ width: '100%' }}>
+                                      <form
+                                          onSubmit={handleSubmit}
+                                          id="update-board"
+                                      >
+                                          <div
+                                              className="form-floating mb-3"
+                                              style={{ textAlign: 'center' }}
+                                          >
+                                              <label htmlFor="board_name">
+                                                  Title
+                                              </label>
+                                              <input
+                                                  onChange={handleFormChange}
+                                                  required
+                                                  type="text"
+                                                  name="board_name"
+                                                  id="board_name"
+                                                  className="form-control"
+                                                  value={formData.board_name}
+                                              />
+                                          </div>
+                                          <div
+                                              className="form-floating mb-3"
+                                              style={{ textAlign: 'center' }}
+                                          >
+                                              <label htmlFor="description">
+                                                  Description
+                                              </label>
+                                              <textarea
+                                                  onChange={handleFormChange}
+                                                  name="description"
+                                                  id="description"
+                                                  className="form-control"
+                                                  value={formData.description}
+                                                  rows="3"
+                                              ></textarea>
+                                          </div>
+                                          <div
+                                              className="form-floating mb-3"
+                                              style={{ textAlign: 'center' }}
+                                          >
+                                              <label htmlFor="cover_photo">
+                                                  Cover photo
+                                              </label>
+                                              <input
+                                                  onChange={handleFormChange}
+                                                  required
+                                                  type="url"
+                                                  name="cover_photo"
+                                                  id="cover_photo"
+                                                  className="form-control"
+                                                  value={formData.cover_photo}
+                                              />
+                                          </div>
+                                      </form>
+                                      <div className="d-flex justify-content-between">
+                                          <button
+                                              className="mb-3"
+                                              style={{ textAlign: 'left' }}
+                                              onClick={() => navigate(-1)}
+                                          >
+                                              Back
+                                          </button>
+                                          <button
+                                              form="update-board"
+                                              className="mb-3"
+                                              style={{ textAlign: 'right' }}
+                                          >
+                                              Update
+                                          </button>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
                       </div>
-                    </div>
-                </div>
+                  </div>
               </div>
-            </div>
           </div>
-        </div>
       </div>
-    </div>
-  );
+  )
 }
 
 export default UpdateBoardForm;
