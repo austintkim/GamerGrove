49c65c01f3f929a373fb30ac3e27a5219367f324
Austin Kim <mambafinisher@gmail.com>
2025-06-02 12:39:01 -0700

    Added date_created and last_update columns to votes (for filtering purposes when displaying on dashboard)

--- api/migrations/013_votes_table.py
+++ api/migrations/013_votes_table.py
@@ -8,12 +8,27 @@ steps = [
         FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE,
         review_id INT NOT NULL,
         FOREIGN KEY (review_id) REFERENCES reviews(id) ON DELETE CASCADE,
-        upvote BOOLEAN
+        upvote BOOLEAN,
+        date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
 
+        CREATE OR REPLACE FUNCTION update_last_update_column()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.last_update = CURRENT_TIMESTAMP;
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+
+        CREATE TRIGGER set_last_update_votes
+        BEFORE UPDATE ON comments
+        FOR EACH ROW
+        EXECUTE FUNCTION update_last_update_column();
         """,
         """
         DROP TABLE votes;
+        DROP FUNCTION update_last_update_column;
         """
     ],
 
