b94a508fe8dd6464bb5969b2b75b55bde4dc79a6
Austin Kim <mambafinisher@gmail.com>
2025-07-02 12:00:00 -0700

    Moved all eslint and prettier dependencies into ghi

---
+++ ghi/eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+	{
+		files: ['**/*.{js,mjs,cjs,jsx}'],
+		languageOptions: {
+			globals: globals.browser,
+		},
+		plugins: {
+			js,
+			prettier: pluginPrettier,
+			react: pluginReact,
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...pluginReact.configs.flat.recommended.rules,
+			'prettier/prettier': 'error',
+		},
+	},
+]);
