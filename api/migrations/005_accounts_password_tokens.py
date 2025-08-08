f5e3a9a5438984a42a82b9bad5c90431219ad0d4
Austin Kim <mambafinisher@gmail.com>
2025-08-08 12:03:39 -0700

    Create accounts_password_tokens table to store reset password tokens server side

---
+++ api/migrations/005_accounts_password_tokens.py
@@ -0,0 +1,16 @@
+steps = [
+    [
+        """
+        CREATE TABLE accounts_password_tokens(
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) UNIQUE,
+        token_text VARCHAR(255) UNIQUE,
+        time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        used BOOLEAN
+        );
+        """,
+        """
+        DROP TABLE accounts_password_tokens;
+        """,
+    ],
+]
